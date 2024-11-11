694ba38b8342515ae24919134d3970b9c1f21747
Talha Rehman <talharehmanabid1999@gmail.com>
2024-11-11 19:52:32 +0300

    Front end HTML File

--- main.py
+++ main.py
@@ -2,12 +2,22 @@
 from model.database import get_db, Base, engine
 
 from fastapi import FastAPI, Depends, HTTPException, status, WebSocket, WebSocketDisconnect, Query
-
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 # Initialize database
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Serve static files
+app.mount("/templates", StaticFiles(directory="templates"), name="templates")
+
+# Setup templates
+templates = Jinja2Templates(directory="templates")
+
+
 
 # Run the FastAPI application
 if __name__ == "__main__":
